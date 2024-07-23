732ddb76d7dd95380a7b07bcbeee9eb1195484f3
Asad Ullah <asadullahkhan27235426@gmail.com>
2024-07-24 03:19:03 +0500

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,15 @@ import routes from './controllers/news-controller';
 import 'dotenv/config'
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+import cors from 'cors'
 
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use('/api', routes);
