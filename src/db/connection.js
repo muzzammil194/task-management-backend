7bb9b454da8d777dfbaaeefbe297afe8dff772f0
muzzammil shaikh <muzzammil194@gmail.com>
2024-07-23 22:13:42 +0500

    fix

---
+++ src/db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+// Create the connection pool. The pool-specific settings are the defaults
+const database_connection = mysql.createPool({
+  host: process.env["DB_HOST"],
+  port: process.env["DB_PORT"],
+  user: process.env["DB_USER"],
+  password: process.env["DB_PASSWORD"] ,
+  database: process.env["DB_DATABASE"],
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = database_connection;
--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -1,71 +1,20 @@
 import moment from 'moment';
 import { INews } from '../models/task.model';
 import { v4 as uuidv4 } from 'uuid';
+const  database_connection   = require("../db/connection")
 
 export class newsService {
-  private tasks: INews[] = [];
-
   postNews = async (task: INews): Promise<any> => {
     try {
-      if (task.creationDate < task.dueDate) {
-        task.id = uuidv4();
-        task.assignedTo = task.assignedTo.toString().toLowerCase().trim();
-        task.category = task.category.toString().toLowerCase().trim();
-        this.tasks.push(task);
-        return task;
-      }
     } catch (ex) {
       return ex;
     }
   }
   getNews = async (assignedTo?: string, category?: string): Promise<any> => {
     try {
-      if (!assignedTo && !category) {
-        const response = this.tasks.map(value => {
-          value.creationDate = moment.unix(parseInt(value.creationDate)).format("YYYY-MM-DD hh:mm:ss")
-          value.dueDate = moment.unix(parseInt(value.dueDate)).format("YYYY-MM-DD hh:mm:ss")
-          return value;
-        })
-        return response;
-      }
-      else if (assignedTo && !category) {
-        const response = this.tasks.filter(filteredValue => {
-          return filteredValue.assignedTo.toLowerCase().trim() === assignedTo.toLowerCase().trim()
-        })
-          .map(value => {
-            value.creationDate = moment.unix(parseInt(value.creationDate)).format("YYYY-MM-DD hh:mm:ss")
-            value.dueDate = moment.unix(parseInt(value.dueDate)).format("YYYY-MM-DD hh:mm:ss")
-            return value;
-          })
-        return response;
-      }
-      else if (!assignedTo && category) {
-        const response = this.tasks.filter(filteredValue => {
-          return filteredValue.category.toLowerCase().trim() == category.toLowerCase().trim()
-        })
-          .map(value => {
-            value.creationDate = moment.unix(parseInt(value.creationDate)).format("YYYY-MM-DD hh:mm:ss")
-            value.dueDate = moment.unix(parseInt(value.dueDate)).format("YYYY-MM-DD hh:mm:ss")
-            return value;
-          })
-        return response;
-      }
-      return "Please filter assignedTo or category to only one value at a time ";
+      return "";
     } catch (ex) {
       return ex;
-    }
-  }
-
-  existOrNotEmpty = async (id:string) => {
-    try {
-      if(this.tasks.length>0){
-        return this.tasks.filter(item => item.id == id);
-      }
-      else{
-        return 'data not found'
-      }
-    } catch (ex) {
-
     }
   }
 }
